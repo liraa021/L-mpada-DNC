ca411daf12d82383b54e2ab82a0b26c3a009def5
liraa021 <rodrigolira94@gmail.com>
2023-07-28 13:46:58 -0300

    Praticando JS

---
+++ lampada.js
@@ -0,0 +1,23 @@
+var lamp = window.document.getElementById("lampada");
+
+function estaQuebrada() {
+  return lamp.src.indexOf("quebrada") > -1;
+}
+
+function Ligar() {
+  if (!estaQuebrada()) {
+    lamp.src = "Imagens/ligada.svg";
+  }
+}
+
+function Desligar() {
+  if (!estaQuebrada()) {
+    lamp.src = "Imagens/desligada.svg";
+  }
+}
+
+lamp.addEventListener("click", Quebrar);
+
+function Quebrar() {
+  lamp.src = "Imagens/quebrada.svg";
+}
